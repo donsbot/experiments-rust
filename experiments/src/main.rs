376fefc3dc6128ccf713c3d12df59d22f8194b1d
Don Stewart <dons@fb.com>
2020-06-07 11:40:26 +0100

    Parsing, folds, ...

--- experiments/src/main.rs
+++ experiments/src/main.rs
@@ -1,6 +1,26 @@
-fn main() {
-    eprintln!("{}", gcd(4,16));
+use std::str::FromStr;
 
+fn main() {
+    let mut ns: Vec<u64> = Vec::new();
+
+    for arg in std::env::args().skip(1) {
+        let i = u64::from_str(&arg);
+        let r = i.expect("Parser error on numeric argument");
+        ns.push(r);
+    }
+
+    if ns.is_empty() {
+        eprintln!("Usage: gcd NUMBER ...");
+        std::process::exit(1);
+    }
+
+    // foldl1 over args
+    let mut r = ns[0]; // there's at least one argument
+    for p in &ns[1..] {
+        r = gcd(r, *p);
+    }
+
+    println!("The GCD of {:?} is {}", ns, r);
 }
 
 fn gcd(mut n: u64, mut m: u64) -> u64 {
@@ -12,4 +32,10 @@ fn gcd(mut n: u64, mut m: u64) -> u64 {
         m %= n;
     }
     n
+}
+
+#[test]
+fn test_gcd() {
+    assert_eq!(gcd(14, 15), 1);
+    assert_eq!(gcd(2 * 3 * 5 * 11 * 17, 3 * 77 * 11 * 13 * 19), 3 * 11);
 }
