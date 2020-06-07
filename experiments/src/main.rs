59df8fc2dd93a30949a32009fd157d872d58d857
Don Stewart <dons@fb.com>
2020-06-07 11:18:50 +0100

    Check clippy/ rustfmt are working

--- experiments/src/main.rs
+++ experiments/src/main.rs
@@ -1,8 +1,15 @@
 fn main() {
-    eprintln!("{}", f(4));
+    eprintln!("{}", gcd(4,16));
+
 }
 
-fn f(mut n : u64) -> u64 {
-    { n += 3;
-      n*2 }
+fn gcd(mut n: u64, mut m: u64) -> u64 {
+    assert!(n != 0 && m != 0);
+    while m != 0 {
+        if m < n {
+            std::mem::swap(&mut m, &mut n);
+        }
+        m %= n;
+    }
+    n
 }
