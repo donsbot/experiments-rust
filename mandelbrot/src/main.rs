1587e43c5d6623bdc7810fca33d7c4e4a9d4f05e
Don Stewart <dons@fb.com>
2020-06-10 14:54:25 +0100

    rustfmt

--- mandelbrot/src/main.rs
+++ mandelbrot/src/main.rs
@@ -1,10 +1,11 @@
-extern crate num;
 extern crate image;
+extern crate num;
 
-use image::ColorType;
 use image::png::PNGEncoder;
-use std::fs::File;
+use image::ColorType;
 use num::Complex;
+use std::fs::File;
+use std::io::Write;
 use std::str::FromStr;
 
 fn main() {
@@ -27,25 +28,25 @@ fn parse_pair<T: FromStr>(s: &str, sep: char) -> Option<(T, T)> {
 fn parse_complex(s: &str) -> Option<Complex<f64>> {
     match parse_pair(s, ',') {
         Some((re, im)) => Some(Complex { re, im }),
-        None => None
+        None => None,
     }
 }
 
 #[test]
 fn test_parse_pair() {
     let sep = ',';
-    assert_eq!(parse_pair::<i32>("",        sep), None);
-    assert_eq!(parse_pair::<i32>("10",      sep), None);
-    assert_eq!(parse_pair::<i32>(",10",     sep), None); // no missing
-    assert_eq!(parse_pair::<i32>("10,20",   sep), Some((10,20)));
-    assert_ne!(parse_pair::<i32>("10,20",   sep), Some((10,10))); // equality
+    assert_eq!(parse_pair::<i32>("", sep), None);
+    assert_eq!(parse_pair::<i32>("10", sep), None);
+    assert_eq!(parse_pair::<i32>(",10", sep), None); // no missing
+    assert_eq!(parse_pair::<i32>("10,20", sep), Some((10, 20)));
+    assert_ne!(parse_pair::<i32>("10,20", sep), Some((10, 10))); // equality
     assert_eq!(parse_pair::<i32>("10,20xy", sep), None); // no trailing
 }
 
 #[allow(dead_code)]
 fn escape_time(c: Complex<f64>, limit: u32) -> Option<u32> {
     let mut z = Complex { re: 0.0, im: 0.0 };
-    for i in 0 .. limit {
+    for i in 0..limit {
         z = z * z + c;
         if z.norm_sqr() > 4.0 {
             return Some(i);
@@ -77,56 +78,67 @@ fn square_loop(mut x: f64) {
 }
 
 #[allow(dead_code)]
-fn pixel_to_point(bounds: (usize, usize),
-                  pixel: (usize, usize),
-                  upper_left: Complex<f64>,
-                  lower_right: Complex<f64>)
-    -> Complex<f64>
-{
-    let (w,h) = (lower_right.re - upper_left.re,
-                 upper_left.im - lower_right.im);
-    Complex{
+fn pixel_to_point(
+    bounds: (usize, usize),
+    pixel: (usize, usize),
+    upper_left: Complex<f64>,
+    lower_right: Complex<f64>,
+) -> Complex<f64> {
+    let (w, h) = (
+        lower_right.re - upper_left.re,
+        upper_left.im - lower_right.im,
+    );
+    Complex {
         re: upper_left.re + pixel.0 as f64 * w / bounds.0 as f64,
-        im: upper_left.im - pixel.1 as f64 * h / bounds.1 as f64
+        im: upper_left.im - pixel.1 as f64 * h / bounds.1 as f64,
     }
 }
 
 #[test]
 fn test_pixel_to_point() {
-    assert_eq!(pixel_to_point((100,100), (25,75),
-                    Complex { re: -1.0, im:  1.0 },
-                    Complex { re:  1.0, im: -1.0 }),
-                Complex { re: -0.5, im: -0.5 });
+    assert_eq!(
+        pixel_to_point(
+            (100, 100),
+            (25, 75),
+            Complex { re: -1.0, im: 1.0 },
+            Complex { re: 1.0, im: -1.0 }
+        ),
+        Complex { re: -0.5, im: -0.5 }
+    );
 }
 
-fn render(pixels: &mut [u8],
-          bounds: (usize, usize),
-          upper_left: Complex<f64>,
-          lower_right: Complex<f64>)
-{
+fn render(
+    pixels: &mut [u8],
+    bounds: (usize, usize),
+    upper_left: Complex<f64>,
+    lower_right: Complex<f64>,
+) {
     assert!(pixels.len() == bounds.0 * bounds.1);
 
-    for row in 0 .. bounds.1 {
-        for column in 0 .. bounds.0 {
-            let point = pixel_to_point(bounds, (column, row),
-                                            upper_left, lower_right);
-            pixels[row * bounds.0 + column] =
-                match escape_time(point, 255) {
-                    None => 0,
-                    Some(count) => 255 - count as u8
-                }
+    for row in 0..bounds.1 {
+        for column in 0..bounds.0 {
+            let point = pixel_to_point(bounds, (column, row), upper_left, lower_right);
+            pixels[row * bounds.0 + column] = match escape_time(point, 255) {
+                None => 0,
+                Some(count) => 255 - count as u8,
+            }
         }
     }
 }
 
-fn write_image(filename: &str, pixels: &[u8], bounds: (usize, usize))
-    -> Result<(), std::io::Error>
-{
+fn write_image(
+    filename: &str,
+    pixels: &[u8],
+    bounds: (usize, usize),
+) -> Result<(), std::io::Error> {
     let output = File::create(filename)?;
 
     let encoder = PNGEncoder::new(output);
-    encoder.encode(&pixels,
-                    bounds.0 as u32, bounds.1 as u32,
-                    ColorType::Gray(8))?;
+    encoder.encode(
+        &pixels,
+        bounds.0 as u32,
+        bounds.1 as u32,
+        ColorType::Gray(8),
+    )?;
     Ok(())
 }
