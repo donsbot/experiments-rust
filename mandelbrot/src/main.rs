9ea6a538ebdcf47ea1ced52e8b46b872ac7d4678
Don Stewart <dons@fb.com>
2020-06-10 14:53:28 +0100

    Add image writing

--- mandelbrot/src/main.rs
+++ mandelbrot/src/main.rs
@@ -1,4 +1,9 @@
 extern crate num;
+extern crate image;
+
+use image::ColorType;
+use image::png::PNGEncoder;
+use std::fs::File;
 use num::Complex;
 use std::str::FromStr;
 
@@ -84,4 +89,44 @@ fn pixel_to_point(bounds: (usize, usize),
         re: upper_left.re + pixel.0 as f64 * w / bounds.0 as f64,
         im: upper_left.im - pixel.1 as f64 * h / bounds.1 as f64
     }
+}
+
+#[test]
+fn test_pixel_to_point() {
+    assert_eq!(pixel_to_point((100,100), (25,75),
+                    Complex { re: -1.0, im:  1.0 },
+                    Complex { re:  1.0, im: -1.0 }),
+                Complex { re: -0.5, im: -0.5 });
+}
+
+fn render(pixels: &mut [u8],
+          bounds: (usize, usize),
+          upper_left: Complex<f64>,
+          lower_right: Complex<f64>)
+{
+    assert!(pixels.len() == bounds.0 * bounds.1);
+
+    for row in 0 .. bounds.1 {
+        for column in 0 .. bounds.0 {
+            let point = pixel_to_point(bounds, (column, row),
+                                            upper_left, lower_right);
+            pixels[row * bounds.0 + column] =
+                match escape_time(point, 255) {
+                    None => 0,
+                    Some(count) => 255 - count as u8
+                }
+        }
+    }
+}
+
+fn write_image(filename: &str, pixels: &[u8], bounds: (usize, usize))
+    -> Result<(), std::io::Error>
+{
+    let output = File::create(filename)?;
+
+    let encoder = PNGEncoder::new(output);
+    encoder.encode(&pixels,
+                    bounds.0 as u32, bounds.1 as u32,
+                    ColorType::Gray(8))?;
+    Ok(())
 }
