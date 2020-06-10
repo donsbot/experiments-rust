56281c8722cf4f35388c76ff6bd3aa4cd1d8354a
Don Stewart <dons@fb.com>
2020-06-10 15:03:40 +0100

    Formatting

--- mandelbrot/src/main.rs
+++ mandelbrot/src/main.rs
@@ -12,23 +12,22 @@ fn main() {
 
     if args.len() != 5 {
         eprintln!("Usage: mandelbrot FILE PIXELS UPPERLEFT LOWERRIGHT");
-        eprintln!("Example: {} mandel.png 1000x750 -1.20,0.35 -1,0.20",args[0]);
+        eprintln!(
+            "Example: {} mandel.png 1000x750 -1.20,0.35 -1,0.20",
+            args[0]
+        );
         std::process::exit(1);
     }
 
-    let bounds = parse_pair(&args[2], 'x')
-        .expect("error parsing image dimensions");
-    let upper_left = parse_complex(&args[3])
-        .expect("error parsing upper left corner point");
-    let lower_right = parse_complex(&args[4])
-        .expect("error parsing lower right corner point");
+    let bounds = parse_pair(&args[2], 'x').expect("error parsing image dimensions");
+    let upper_left = parse_complex(&args[3]).expect("error parsing upper left corner point");
+    let lower_right = parse_complex(&args[4]).expect("error parsing lower right corner point");
 
     let mut pixels = vec![0; bounds.0 * bounds.1];
 
     render(&mut pixels, bounds, upper_left, lower_right);
 
-    write_image(&args[1], &pixels, bounds)
-        .expect("error writing PNG file");
+    write_image(&args[1], &pixels, bounds).expect("error writing PNG file");
 }
 
 #[allow(dead_code)]
