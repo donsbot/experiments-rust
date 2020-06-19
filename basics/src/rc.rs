ff605fad1f9581434bf9169462bad0a076102fb5
Don Stewart <dons@fb.com>
2020-06-19 12:56:35 +0100

    Can't share and mutate

---
+++ basics/src/rc.rs
@@ -0,0 +1,22 @@
+// Rc and Arc
+//
+// Rc: Refefence Count
+// Arc: Atomic Reference Count
+//
+use std::rc::Rc;
+
+fn main() {
+    // construct a reference counted pointer
+    let s: Rc<String> = Rc::new("foo".to_string());
+    let t: Rc<String> = s.clone();
+    let u: Rc<String> = s.clone();
+
+    // 3 stack allocated pointers to a heap allocated ref-count box + string ref (unboxed) 
+    // all refer to the same block
+    // can't be shared and mutable
+    // s.push_str(" noodles");
+
+
+
+
+}
