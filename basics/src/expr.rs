c36870d3e998d550904a7febc020fb09b754c6ec
Don Stewart <dons@fb.com>
2020-06-21 12:29:33 +0100

    No tail recursion sob sob

--- basics/src/expr.rs
+++ basics/src/expr.rs
@@ -6,4 +6,50 @@ fn main() {
         _ => {"anything else"}
     };
     println!("{}",x);
+
+    let lim = 100;
+    let y = f2(lim);
+    println!("{}", y);
+    let y = f1(lim);
+    println!("{}", y);
+
+    let lim = 1000000000;
+    let y = f2(lim);
+    println!("{}", y);
+    let y = f1(lim);
+    println!("{}", y);
+
+}
+
+// interesting. no tail recursion
+// the obvious way:
+fn f1(n: i64) -> i64 {
+    go(n,0)
+}
+
+fn go(n:i64, acc:i64) -> i64 {
+    if n <= 0 {
+        acc
+    } else {
+        go(n-1, acc + n)
+    }
+}
+
+// explicit loop with mutable params
+fn f2(n: i64) -> i64 {
+    go2(n)
+}
+
+fn go2(n:i64) -> i64 {
+    let mut acc = 0;
+    let mut m   = n;
+    loop {
+        if m <= 0 {
+            return acc;
+        } else {
+            acc += m;
+            m -= 1;
+        }
+    }
+
 }
