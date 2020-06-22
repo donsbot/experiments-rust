1cf1c7bc38094e8253878c2238787e19c377d18b
Don Stewart <dons@fb.com>
2020-06-22 11:05:52 +0100

    Panics of various sorts

---
+++ basics/src/panic.rs
@@ -0,0 +1,26 @@
+// rustc -C panic=abbort panic.rs
+#[allow(unreachable_code)]
+fn main() {
+    // unwinding
+    {
+        println!("{}", pirate_share(100, 15));
+        println!("{}", pirate_share(100, 1));
+    }
+
+    panic!("{:?}", panic!()); // explicit panic.
+
+    // interesting:
+    fix();
+
+    {
+        println!("{}", pirate_share(100, 0)); // panicked at 'attempt to divide by zero'
+    }
+}
+
+fn fix() -> ! {
+    loop {}
+}
+
+fn pirate_share(total: u64, crew_size: usize) -> u64 {
+    (total / 2) / crew_size as u64
+}
