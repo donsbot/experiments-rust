41763cd0f1b67d730051c544c677e2691032d4d8
Don Stewart <dons@fb.com>
2020-06-24 14:18:45 +0100

    Some more struct stuff

--- basics/src/struct.rs
+++ basics/src/struct.rs
@@ -3,9 +3,26 @@
 // types have CamelCase. fields and functions have snake case
 //
 
-pub struct GSM { // greyscale map
+#[derive(Clone)]
+struct GSM { // greyscale map
     pixels: Vec<u8>,
     size: (usize, usize),
+}
+
+// tuple-like struct. product type with a newtype and indexing
+#[derive(Debug)]
+struct P(usize, usize, usize);
+
+// hehey you can use them as newtypes
+#[allow(dead_code)]
+struct T(Vec<u8>);
+
+// empty data types, wohoo
+#[allow(dead_code)]
+struct U;
+
+fn mk_p(a: usize, b: usize, c: usize) -> P {
+    P(a,b,c)
 }
 
 fn mk_gsm(w: usize, h: usize) -> GSM {
@@ -22,7 +39,21 @@ pub fn main() {
     let w = 1024;
     let h = 576;
 
-    let gsm = mk_gsm(w,h);
+    let a = mk_gsm(w,h);
+    let b = mk_gsm(w/2, h/2);
 
-    println!("{}", gsm.pixels.len());
+    let v = vec![0;0];
+    let c = GSM { pixels: v, .. b };
+
+    // move pixels from 'a' , not copied!
+    let d = GSM { pixels: a.pixels.clone(), .. a };
+
+    println!("{}", a.pixels.len());
+    println!("{}", b.pixels.len());
+    println!("{}", c.pixels.len());
+    println!("{}", d.pixels.len());
+
+    let a = mk_p(1,2,3);
+    println!("{:?}", a);
+    println!("{:?}", a.1);
 }
