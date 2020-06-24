09aebc4d83551fb05a4cda084cda5682ee45985c
Don Stewart <dons@fb.com>
2020-06-24 12:12:35 +0100

    Update types, drop the nested module bit

--- difference-list/src/lib.rs
+++ difference-list/src/lib.rs
@@ -1,5 +1,3 @@
-mod dlist {
-
 // A difference list is a function that, given a list, returns the original contents of the
 // difference list prepended to the given list.
 //
@@ -7,20 +5,21 @@ mod dlist {
 // very useful for append-heavy uses (esp. left-nested uses of append operations), such as logging
 // and pretty printing.
 //
-    // pub struct DList<A> { unDL: &(dyn Fn(&[A]) -> &[A]) }
+//
+// pub struct DList<A> { unDL: &(dyn Fn(&[A]) -> &[A]) }
 
-    pub fn id<A>(a: A) -> A {
-        a
-    }
-
-    // pub fn empty<A>() -> DList<A> {
-    //     DList { unDL: &id }
-   //  }
-
+pub fn id<A>(a: A) -> A {
+    a
 }
+
+// pub fn empty<A>() -> DList<A> {
+//     DList { unDL: &id }
+//  }
 
 #[cfg(test)]
 mod tests {
     #[test]
-    assert_eq!(super::id(1), 1);
+    fn test_id() {
+        assert_eq!(super::id(1), 1);
+    }
 }
