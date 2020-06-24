0fa5238304d18fd67a5e50c10c90c3d501172538
Don Stewart <dons@fb.com>
2020-06-24 13:18:49 +0100

    Tweaks

--- difference-list/src/lib.rs
+++ difference-list/src/lib.rs
@@ -1,25 +1,26 @@
-// A difference list is a function that, given a list, returns the original contents of the
-// difference list prepended to the given list.
-//
-// This structure supports /O(1)/ append and snoc operations on lists, making it
-// very useful for append-heavy uses (esp. left-nested uses of append operations), such as logging
-// and pretty printing.
-//
-//
+///
+/// This structure supports /O(1)/ append and snoc operations on lists, making it
+/// very useful for append-heavy uses (esp. left-nested uses of append operations), such as logging
+/// and pretty printing.
+///
+pub mod dlist {
+
+    use std::marker::PhantomData;
+
+    /// The type of difference lists of A
+    pub struct DList<A> {
+        phantom: PhantomData<A>,
+    }
+
 // pub struct DList<A> { unDL: &(dyn Fn(&[A]) -> &[A]) }
 
-pub fn id<A>(a: A) -> A {
-    a
-}
-
-// pub fn empty<A>() -> DList<A> {
-//     DList { unDL: &id }
-//  }
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn test_id() {
-        assert_eq!(super::id(1), 1);
+    pub fn empty<A>() -> DList<A> {
+        DList{phantom: PhantomData}
     }
+
+    #[allow(dead_code)]
+    fn id<A>(a: A) -> A {
+        a
+    }
+
 }
---
+++ difference-list/src/tests.rs
@@ -0,0 +1,15 @@
+#[cfg(test)]
+
+use dlist::{id};
+
+mod tests {
+    #[test]
+    fn test_id1() {
+/*
+        assert_eq!(dlist::id(1), 1);
+        assert_eq!(dlist::id(()), ());
+        assert_eq!(dlist::id(""), "");
+        assert_eq!(dlist::id([1,2,3]), [1,2,3]);
+*/
+    }
+}
