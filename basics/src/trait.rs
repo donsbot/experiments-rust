f559b20eb6198b33fa60ec9573f6147c104e1576
Don Stewart <dons@fb.com>
2020-06-27 16:19:29 +0100

    JSObject ppr

--- basics/src/trait.rs
+++ basics/src/trait.rs
@@ -1,37 +1,31 @@
 // barebones 2008-era JSON pretty printer
-/*
-data JSValue
-    | JSObject   (JSObject JSValue)
-*/
 
 extern crate num;
 extern crate pretty;
 
+use num::rational::{Ratio, Rational64};
 use pretty::RcDoc as R;
-use num::rational::{Ratio,Rational64};
 
-#[derive(Debug,PartialEq)]
+//
+// type of JSON values
+//
+#[derive(Debug, PartialEq)]
 pub enum JSValue {
-      JSNull
-    , JSBool(bool)
-    , JSRational(Rational64)
-    , JSString(String)
-    , JSArray(Vec<JSValue>)
+    JSNull,
+    JSBool(bool),
+    JSRational(Rational64),
+    JSString(String),
+    JSArray(Vec<JSValue>),
+    JSObject(Vec<(JSLabel, JSValue)>),
 }
-//    , JSObject(JSAssocVec)
 
-#[derive(Debug,PartialEq)]
+#[derive(Debug, PartialEq)]
 pub struct JSLabel(String);
 
-#[derive(Debug,PartialEq)]
-pub struct JSAssocVec(Vec<(JSLabel,JSValue)>);
-
-/* pretty printing */
-
+// pretty printing
 impl JSValue {
-
     pub fn to_doc(&self) -> R<()> {
-		pp_value(self)
+        pp_value(self)
     }
 
     pub fn to_pretty(&self, width: usize) -> String {
@@ -39,17 +33,18 @@ impl JSValue {
         self.to_doc().render(width, &mut w).unwrap();
         String::from_utf8(w).unwrap()
     }
-
 }
-            
+
+// Print a generic value
 fn pp_value(v: &JSValue) -> R<()> {
-	match *v {
-		J::JSNull => pp_null(),
-		J::JSBool(b) => pp_bool(b),
-		J::JSRational(r) => pp_number(r),
-		J::JSString(ref s) => pp_string(s),
-		J::JSArray(ref v) => pp_array(v),
-	}
+    match *v {
+        J::JSNull => pp_null(),
+        J::JSBool(b) => pp_bool(b),
+        J::JSRational(r) => pp_number(r),
+        J::JSString(ref s) => pp_string(s),
+        J::JSArray(ref v) => pp_array(v),
+        J::JSObject(ref v) => pp_object(v),
+    }
 }
 
 fn pp_null<'a>() -> R<'a, ()> {
@@ -57,9 +52,7 @@ fn pp_null<'a>() -> R<'a, ()> {
 }
 
 fn pp_bool<'a>(v: bool) -> R<'a, ()> {
-    R::text(
-        if v { "true" } else { "false" }
-    )
+    R::text(if v { "true" } else { "false" })
 }
 
 fn pp_number<'a>(x: Rational64) -> R<'a, ()> {
@@ -69,72 +62,66 @@ fn pp_number<'a>(x: Rational64) -> R<'a, ()> {
     } else {
         panic!("can't format: {:?}", x)
     }
-
 }
 
 fn pp_string(s: &str) -> R<()> {
-	let ts: Vec<R<()>> =
-			 s.chars()
-			  .map(pp_char)
-              .collect();
+    let ts: Vec<R<()>> = s.chars().map(pp_char).collect();
     double_quotes(R::concat(ts))
 }
 
-fn pp_char<'a> (c: char) -> R<'a,()> {
-	match c {
-		'\\' => R::text("\\\\"),
-		'"'  => R::text(r#"\\""#),
-		c if c.is_control() => {
-				let s : String = c.escape_unicode().collect();
-				R::text(s)
- 		},
-		_    => R::text(c.to_string())
-	}
+fn pp_char<'a>(c: char) -> R<'a, ()> {
+    match c {
+        '\\' => R::text("\\\\"),
+        '"' => R::text(r#"\\""#),
+        c if c.is_control() => {
+            let s: String = c.escape_unicode().collect();
+            R::text(s)
+        }
+        _ => R::text(c.to_string()),
+    }
 }
 
 fn pp_array(vs: &[JSValue]) -> R<()> {
-
-	let ts: Vec<R<()>> =
-				vs.iter()
-			   .map(pp_value)
-               .collect();
-	brackets(	
-		R::intersperse(ts,R::text(COMMA))
-	)
+    let ts: Vec<R<()>> = vs.iter().map(pp_value).collect();
+    brackets(R::intersperse(ts, R::text(COMMA)))
 }
 
-const DOUBLE_QUOTE : &str = &r#"""#;
-const LEFT_BRACKET : &str = &r#"["#;
+fn pp_object<'a>(vs: &'a Vec<(JSLabel, JSValue)>) -> R<'a, ()> {
+    let ts: Vec<R<()>> = vs.iter().map(pp_field).collect();
+    braces(R::intersperse(ts, R::text(COMMA)))
+}
+
+fn pp_field<'a>((JSLabel(k), v): &'a (JSLabel, JSValue)) -> R<'a, ()> {
+    pp_string(k)
+        .append(R::text(":"))
+        .append(R::space())
+        .append(pp_value(v))
+}
+
+const DOUBLE_QUOTE: &str = &r#"""#;
+const LEFT_BRACKET: &str = &r#"["#;
 const RIGHT_BRACKET: &str = &r#"]"#;
-const COMMA        : &str = &r#","#;
+const LEFT_BRACE: &str = &r#"{"#;
+const RIGHT_BRACE: &str = &r#"}"#;
+const COMMA: &str = &r#","#;
 
-fn double_quotes(d : R<()>) -> R<()> {
-	R::text(DOUBLE_QUOTE)
-		.append(d)
-		.append(R::text(DOUBLE_QUOTE))
+fn double_quotes(d: R<()>) -> R<()> {
+    R::text(DOUBLE_QUOTE)
+        .append(d)
+        .append(R::text(DOUBLE_QUOTE))
 }
 
-fn brackets(d : R<()>) -> R<()> {
-	R::text(LEFT_BRACKET)
-		.append(d)
-		.append(R::text(RIGHT_BRACKET))
+fn brackets(d: R<()>) -> R<()> {
+    R::text(LEFT_BRACKET)
+        .append(d)
+        .append(R::text(RIGHT_BRACKET))
 }
 
-/*
-List(ref xs) =>
-                RcDoc::text("(")
-                    .append(RcDoc::intersperse(xs.into_iter().map(|x| x.to_doc()), Doc::line()).nest(1).group())
-                    .append(RcDoc::text(")"))
-*/
+fn braces(d: R<()>) -> R<()> {
+    R::text(LEFT_BRACE).append(d).append(R::text(RIGHT_BRACE))
+}
 
-/*
-
-pp_value v        = case v of
-    JSArray vs   -> pp_array vs
-    JSObject xs  -> pp_js_object xs
-*/
-
-/* test */
+// test
 
 use self::JSValue as J;
 
@@ -147,9 +134,21 @@ pub fn main() {
     assert_eq!("12", J::JSRational(n).to_pretty(80));
 
     assert_eq!(r#""foo""#, J::JSString("foo".to_string()).to_pretty(80));
-    assert_eq!(r#""f\\"oo""#, J::JSString(r#"f"oo"#.to_string()).to_pretty(80));
-    assert_eq!(r#""f❤o\u{9c}""#, J::JSString(r#"f❤o"#.to_string()).to_pretty(80));
+    assert_eq!(
+        r#""f\\"oo""#,
+        J::JSString(r#"f"oo"#.to_string()).to_pretty(80)
+    );
+    assert_eq!(
+        r#""f❤o\u{9c}""#,
+        J::JSString(r#"f❤o"#.to_string()).to_pretty(80)
+    );
 
-	let v = J::JSArray(vec![J::JSNull,J::JSBool(true),J::JSBool(false)]);
+    let v = J::JSArray(vec![J::JSNull, J::JSBool(true), J::JSBool(false)]);
     assert_eq!("[null,true,false]", v.to_pretty(80));
+
+    let o = J::JSObject(vec![
+        (JSLabel("A".to_string()), J::JSNull),
+        (JSLabel("B".to_string()), J::JSBool(true)),
+    ]);
+    assert_eq!(r#"{"A": null,"B": true}"#, o.to_pretty(80));
 }
