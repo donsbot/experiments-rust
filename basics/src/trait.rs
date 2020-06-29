d8a490d9845af83422fd5413dfdddd29eb43d359
Don Stewart <dons@fb.com>
2020-06-29 11:49:39 +0100

    Playing with trait parameters

--- basics/src/trait.rs
+++ basics/src/trait.rs
@@ -1,14 +1,34 @@
 // pg 245, traits
 
 trait V {
-    fn is_good(&self) -> bool;
+    fn is_good(&self) -> bool {
+        false // default impl
+    }
 }
 
-impl V for i128 { fn is_good(&self) -> bool { *self > 0 } }
-impl V for i64 { fn is_good(&self) -> bool { *self > 0 } }
-impl V for i32 { fn is_good(&self) -> bool { *self > 0 } }
-impl V for i16 { fn is_good(&self) -> bool { *self > 0 } }
-impl V for i8 { fn is_good(&self) -> bool { *self > 0 } }
+extern crate num_traits;
+use num_traits::identities as I;
+
+/* whoo num class */
+impl<O: PartialOrd + I::Zero> V for O {
+    #[inline]
+    fn is_good(&self) -> bool {
+        *self > I::zero()
+    }
+}
+
+/*
+#[inline]
+fn generic_gtz<T: PartialOrd>(v: T, zero: T) -> bool {
+    v > zero
+}
+
+impl V for i128 { fn is_good(&self) -> bool {  generic_gtz(*self, 0) } }
+impl V for i64  { fn is_good(&self) -> bool {  generic_gtz(*self, 0) } }
+impl V for i32  { fn is_good(&self) -> bool {  generic_gtz(*self, 0) } }
+impl V for i16  { fn is_good(&self) -> bool {  generic_gtz(*self, 0) } }
+impl V for i8   { fn is_good(&self) -> bool {  generic_gtz(*self, 0) } }
+*/
 
 pub fn main() {
     println!("{}",V::is_good(&1i8));
