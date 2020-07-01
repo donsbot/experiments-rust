1cde06cf7a12103696eecbdb8ac81e897cc26383
Don Stewart <dons@fb.com>
2020-07-01 17:22:13 +0100

    last and clean up Stream definition

--- basics/src/stream.rs
+++ basics/src/stream.rs
@@ -11,15 +11,15 @@ enum Step<'a,S,A> {
 
 // data Stream a = forall s. Stream (s -> (Step s a)) s
 struct Stream<'a, S, A> {
-    f: Box<dyn Fn(&S) -> Step<'a,S,A> + 'a>,
-    st: &'a S
+    next: Box<dyn Fn(&S) -> Step<'a,S,A> + 'a>,
+    state: &'a S
 }
 
 // Check if a 'Stream' is empty
 fn null<'a,A,S>(s: &Stream<'a,S,A>) -> bool {
-    let mut st1 = s.st;
+    let mut st1 = s.state;
     loop {
-        let r = (s.f)(&st1);
+        let r = (s.next)(&st1);
         match r {
             Step::Yield(..) => { return false }
             Step::Skip(st2) => { st1 = st2 }
@@ -31,8 +31,8 @@ fn null<'a,A,S>(s: &Stream<'a,S,A>) -> bool {
 // The empty stream
 fn empty<'a,A>() -> Stream<'a, (), A> {
     Stream {
-        f: Box::new(|_| Step::Done),
-        st: &()
+        next: Box::new(|_| Step::Done),
+        state: &()
     }
 }
 
@@ -48,17 +48,19 @@ fn singleton<A>(a: &A) -> Stream<bool, A> {
     };
 
     Stream {
-        f: Box::new(step),
-        st: &true
+        next: Box::new(step),
+        state: &true
     }
 }
 
+/*
 enum Either<A,B> {
     Left(A),
     Right(B)
 }
 
 use Either::*;
+*/
 
 /*
 // Concatenate two streams
@@ -88,12 +90,12 @@ fn append<'a,S: 'a ,T: 'a,A>(l: Stream<'a, S, A>
 }
 */
 
-// First element of the 'Stream' or error if empty
+// First element of the 'Stream' or None if empty
 // head :: Monad m => Stream m a -> m a
 fn head<'a,A,S>(s: &Stream<'a,S,A>) -> Option<&'a A> {
-    let mut st1 = s.st;
+    let mut st1 = s.state;
     loop {
-        let r = (s.f)(&st1);
+        let r = (s.next)(&st1);
         match r {
             Step::Yield(x,_) => { return Some(x) }
             Step::Skip(st2) => { st1 = st2 }
@@ -101,6 +103,35 @@ fn head<'a,A,S>(s: &Stream<'a,S,A>) -> Option<&'a A> {
         }
     }
 }
+
+// Last element of the 'Stream' or None if empty
+// last :: Monad m => Stream m a -> m a
+fn last<'a,A,S>(s: &Stream<'a,S,A>) -> Option<&'a A> {
+    let mut st1 = s.state;
+    // we do this as two loops. one that iterates until we find at least one value
+    // the other that then holds the most recent seen one, until it returns
+    let mut result: &A;
+
+    loop {
+        let r = (s.next)(&st1);
+        match r {
+            Step::Yield(x,st2) => { st1 = st2; result = x; break }
+            Step::Skip(st2) => { st1 = st2 }
+            Step::Done => { return None } 
+        }
+    }
+
+    // r is definitely initialized now with a possible result
+    loop {
+        let r = (s.next)(&st1);
+        match r {
+            Step::Yield(y,st2) => { st1 = st2; result = y; }
+            Step::Skip(st2) => { st1 = st2 }
+            Step::Done => { return Some(result); } 
+        }
+    }
+}
+
 
 /* basic tests */
 pub fn main() {
@@ -114,5 +145,6 @@ pub fn main() {
     assert_eq!(false, null(&s2));
 
     assert_ne!(head(&s2),None) ;
-    assert_eq!(head(&s2),Some(&42));
+
+    assert_eq!(head(&s2),last(&s2));
 }
