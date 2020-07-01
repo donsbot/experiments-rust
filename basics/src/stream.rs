14eed34c73f1b60de1d55794485bbeeca9d1d367
Don Stewart <dons@fb.com>
2020-07-01 18:18:45 +0100

    replicate, to generate a stream

--- basics/src/stream.rs
+++ basics/src/stream.rs
@@ -4,22 +4,22 @@
 
 // Result of taking a single step in a stream
 enum Step<'a,S,A> {
-    Yield(&'a A, &'a S),
-    Skip(&'a S),
+    Yield(&'a A, S),
+    Skip(S),
     Done,
 }
 
 // data Stream a = forall s. Stream (s -> (Step s a)) s
 struct Stream<'a, S, A> {
-    next: Box<dyn Fn(&S) -> Step<'a,S,A> + 'a>,
-    state: &'a S
+    next: Box<dyn Fn(S) -> Step<'a,S,A> + 'a>,
+    state: S
 }
 
 // Check if a 'Stream' is empty
-fn null<'a,A,S>(s: &Stream<'a,S,A>) -> bool {
+fn null<'a,A,S: Copy>(s: &Stream<'a,S,A>) -> bool {
     let mut st1 = s.state;
     loop {
-        let r = (s.next)(&st1);
+        let r = (s.next)(st1);
         match r {
             Step::Yield(..) => { return false }
             Step::Skip(st2) => { st1 = st2 }
@@ -32,16 +32,16 @@ fn null<'a,A,S>(s: &Stream<'a,S,A>) -> bool {
 fn empty<'a,A>() -> Stream<'a, (), A> {
     Stream {
         next: Box::new(|_| Step::Done),
-        state: &()
+        state: ()
     }
 }
 
 // A stream with a single element
 fn singleton<A>(a: &A) -> Stream<bool, A> {
 
-    let step = move |b: &bool| {
-        if *b {
-            Step::Yield(a, &false)
+    let step = move |b: bool| {
+        if b {
+            Step::Yield(a, false)
         } else {
             Step::Done
         }
@@ -49,7 +49,7 @@ fn singleton<A>(a: &A) -> Stream<bool, A> {
 
     Stream {
         next: Box::new(step),
-        state: &true
+        state: true
     }
 }
 
@@ -62,40 +62,71 @@ enum Either<A,B> {
 use Either::*;
 */
 
-/*
 // Concatenate two streams
 // (++) :: Monad m => Stream m a -> Stream m a -> Stream m a
-fn append<'a,S: 'a ,T: 'a,A>(l: Stream<'a, S, A>
-                  , r: Stream<'a, T, A>) -> Stream<'a, Either<S,T>, A>
+//
+/*
+fn append<'a, S, T, A>
+    ( l : &Stream<'a,S,A>
+    , r : &Stream<'a,T,A>) -> Stream<'a, Either<S,T>, A>
 {
+
     let step = move |a: Either<S,T>| {
         match a {
             Left(sa) => {
-                let r = (l.f)(sa);
+    //            let r = (l.f)(sa);
                 unimplemented!();
             }
 
             Right(sb) => {
-                let r = (r.f)(sb);
+    //            let r = (r.f)(sb);
                 unimplemented!()
             }
         }
     };
 
     Stream {
-        f: Box::new(step),
-        st: Left(l.st)
+        next: Box::new(step),
+        state: &Left(l.state)
     }
+    unimplemented!();
 
 }
 */
 
+// Yield a 'Stream' of values obtained by running the generator a given number of times
+fn replicate<A>(n: usize, a: &A) -> Stream<usize,A> {
+
+    let step = move |i:usize| {
+        if i == 0 {
+            Step::Done
+        } else {
+            Step::Yield(a, i-1)
+        }
+    };
+
+    Stream {
+        next: Box::new(step),
+        state: n
+    }
+}
+
+/*
+replicateM :: Monad m => Int -> m a -> Stream m a
+{-# INLINE_FUSED replicateM #-}
+replicateM n p = Stream step n
+  where
+    {-# INLINE_INNER step #-}
+    step i | i <= 0    = return Done
+           | otherwise = do { x <- p; return $ Yield x (i-1) }
+*/
+
 // First element of the 'Stream' or None if empty
 // head :: Monad m => Stream m a -> m a
-fn head<'a,A,S>(s: &Stream<'a,S,A>) -> Option<&'a A> {
+fn head<'a,A,S: Copy>(s: &Stream<'a,S,A>) -> Option<&'a A> {
     let mut st1 = s.state;
     loop {
-        let r = (s.next)(&st1);
+        let r = (s.next)(st1);
         match r {
             Step::Yield(x,_) => { return Some(x) }
             Step::Skip(st2) => { st1 = st2 }
@@ -106,14 +137,14 @@ fn head<'a,A,S>(s: &Stream<'a,S,A>) -> Option<&'a A> {
 
 // Last element of the 'Stream' or None if empty
 // last :: Monad m => Stream m a -> m a
-fn last<'a,A,S>(s: &Stream<'a,S,A>) -> Option<&'a A> {
+fn last<'a,A,S: Copy>(s: &Stream<'a,S,A>) -> Option<&'a A> {
     let mut st1 = s.state;
     // we do this as two loops. one that iterates until we find at least one value
     // the other that then holds the most recent seen one, until it returns
     let mut result: &A;
 
     loop {
-        let r = (s.next)(&st1);
+        let r = (s.next)(st1);
         match r {
             Step::Yield(x,st2) => { st1 = st2; result = x; break }
             Step::Skip(st2) => { st1 = st2 }
@@ -123,7 +154,7 @@ fn last<'a,A,S>(s: &Stream<'a,S,A>) -> Option<&'a A> {
 
     // r is definitely initialized now with a possible result
     loop {
-        let r = (s.next)(&st1);
+        let r = (s.next)(st1);
         match r {
             Step::Yield(y,st2) => { st1 = st2; result = y; }
             Step::Skip(st2) => { st1 = st2 }
@@ -138,6 +169,7 @@ pub fn main() {
 
     let s1: Stream<(),char> = empty();
     let s2: Stream<bool,i64> = singleton(&42);
+    let s3: Stream<usize,i64> = replicate(10,&0);
 
     assert_eq!(true, null(&s1));
     assert_eq!(false, null(&s2));
@@ -145,6 +177,6 @@ pub fn main() {
     assert_eq!(false, null(&s2));
 
     assert_ne!(head(&s2),None) ;
-
+    assert_ne!(head(&s2),head(&s3));
     assert_eq!(head(&s2),last(&s2));
 }
