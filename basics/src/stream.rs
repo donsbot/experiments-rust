f2c56696b88b08971c46ebef7626c7021b703b09
Don Stewart <dons@fb.com>
2020-07-01 15:14:48 +0100

    It compiles!

--- basics/src/stream.rs
+++ basics/src/stream.rs
@@ -1,72 +1,57 @@
 // 
 // example of how to do stream fusion types in Rust
 //
-enum Step<S,A> {
-    Yield(A, S),
+enum Step<'a,S,A> {
+    Yield(&'a A, S),
     Skip(S),
     Done,
 }
 
 // data Stream a = forall s. Stream (s -> (Step s a)) s
-struct Stream<S, A> {
-    f: dyn Fn(S) -> Step<S,A>,
-    s: S
+struct Stream<'a, S, A> {
+    f: Box<dyn Fn(S) -> Step<'a,S,A> + 'a>,
+    st: S
 }
 
-fn empty<A>() -> Stream<(),A> {
+fn empty<'a,A>() -> Stream<'a, (), A> {
     Stream {
-        f: &(|_| Step::Done),
-        s: ()
+        f: Box::new(|_| Step::Done),
+        st: ()
     }
 }
 
-/*
-fn const_<A,B>(a: A,_ : B) -> A {
-     a
-}
-*/
-
-/*
-/*
- * can't get this to capture 'x' correctly yet
- */
-fn singleton<'a, A>(x: &A) -> Stream<'a,bool,&A> {
-
+fn singleton<'a, A>(a: &'a A) -> Stream<'a, bool, A> {
     Stream {
-        f: |b| { singleton_helper(b, x) },
-        s: true
-    }
-
-}
-*/
-
-//  fn step<A>(b: bool) -> Step<bool,A> {
-fn singleton_helper<'a, A>(b:bool, x :&A) -> Step<bool,&A> {
-    if b {
-        Step::Yield(x , false)
-    } else {
-        Step::Done
+        f: Box::new(move |b| {
+                    if b {
+                        Step::Yield(a, false)
+                    } else {
+                        Step::Done
+                    }
+                }),
+        st: true
     }
 }
 
-
-/*
-
--- | Check if a 'Stream' is empty
-null :: Monad m => Stream m a -> m Bool
-{-# INLINE_FUSED null #-}
-null (Stream step t) = null_loop t
-  where
-    null_loop s = do
-      r <- step s
-      case r of
-        Yield _ _ -> return False
-        Skip s'   -> null_loop s'
-        Done      -> return True
-*/
+// Check if a 'Stream' is empty
+fn null<'a,A,S>(s: Stream<'a,S,A>) -> bool {
+    let mut st1 = s.st;
+    loop {
+        let r = (s.f)(st1);
+        match r {
+            Step::Yield(..) => { return false }
+            Step::Skip(st2) => { st1 = st2 }
+            Step::Done => { return true }
+        }
+    }
+}
 
 pub fn main() {
 
     let s1: Stream<(),char> = empty();
+    let s2: Stream<bool,i64> = singleton(&42);
+
+    println!("{}", null(s1));
+    println!("{}", null(s2));
 
 }
