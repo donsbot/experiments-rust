9ed7121960a7a8b67a55f33c3619291a725f826d
Don Stewart <dons@fb.com>
2020-07-06 13:21:09 +0100

    Some more iter examples

--- basics/src/iter.rs
+++ basics/src/iter.rs
@@ -25,4 +25,55 @@ pub fn main() {
 
     }
 
+    // generating iterators by references and values
+    #[allow(clippy::into_iter_on_ref)]
+    {
+        use std::collections::BTreeSet;
+        let mut f = BTreeSet::new();
+        f.insert("foos".to_string());
+        f.insert("bars".to_string());
+
+        // some kind of traversal
+        let r = &f;
+        let mut i = r.into_iter(); // shared mut ref
+        assert_eq!(i.next(), Some(&"bars".to_string()));
+        assert_eq!(i.next(), Some(&"foos".to_string()));
+        assert_eq!(i.next(), None);
+
+        let mut i = f.into_iter();
+        assert_eq!(i.next(), Some("bars".to_string()));
+
+        // by value
+    }
+
+    // generics
+    {
+        use std::fmt::Debug;
+
+        fn dump<T, U>(t: T)
+            where T: IntoIterator<Item=U>,
+                  U: Debug
+        {
+            for u in t {
+                println!("{:?}", u);
+            }
+        }
+        let v = vec!["abc","abcd","foo","bar"];
+        dump(v);
+
+    }
+
+    // drain: passes ownership , drops the rest
+    {
+        use std::iter::FromIterator;
+
+        // take a slice
+        let mut outer = "Earth".to_string();
+        let inner = String::from_iter(outer.drain(1..4));
+        assert_eq!(outer, "Eh");
+        assert_eq!(inner, "art");
+
+
+    }
+
 }
