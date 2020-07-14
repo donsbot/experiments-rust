f220c52f5369d8437d5691560f4e8041b9ea42ba
Don Stewart <dons@fb.com>
2020-07-14 10:43:05 +0100

    Some file copying fun

--- basics/src/io.rs
+++ basics/src/io.rs
@@ -8,4 +8,32 @@ pub fn main() {
     println!("{:?}", m);
 
     assert_eq!(false, m.is_dir());
+
+    let mut one = File::open("io.rs").unwrap();
+    let mut two = File::create("/tmp/io.rs").unwrap();
+    copy(&mut one, &mut two).unwrap();
+}
+
+use std::io::{self, Read, Write, ErrorKind};
+
+const DEFAULT_BUF_SIZE: usize = 8 * 1024;
+
+// generic copy of buffers
+pub fn copy<R: ?Sized, W: ?Sized>(reader: &mut R, writer: &mut W)
+    -> io::Result<u64>
+    where R: Read, W: Write
+{
+    let mut buf = [0; DEFAULT_BUF_SIZE];
+    let mut count = 0;
+
+    loop {
+        let len = match reader.read(&mut buf) {
+            Ok(0) => return Ok(count),
+            Ok(len) => len,
+            Err(ref e) if e.kind() == ErrorKind::Interrupted => continue,
+            Err(e) => return Err(e),
+        };
+        writer.write_all(&buf[..len])?;
+        count += len as u64;
+    }
 }
